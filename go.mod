c549ae5d912087b67f8e87daa4bd85c89b53edf0
Fedor B Gorsky <fedor@support-pc.org>
2022-05-20 16:12:35 +0300

    Cloud functions run

--- go.mod
+++ go.mod
@@ -20,12 +20,18 @@ require (
 )
 
 require (
+	github.com/Microsoft/go-winio v0.5.2 // indirect
 	github.com/certifi/gocertifi v0.0.0-20210507211836-431795d63e8d // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.16+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/getsentry/raven-go v0.2.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.10.1 // indirect
@@ -46,6 +52,8 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rs/xid v1.2.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -54,7 +62,7 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.0.0-20211008194852-3b03d305991f // indirect
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
 	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.57.0 // indirect
--- models/cloud_function.go
+++ models/cloud_function.go
@@ -1,15 +1,55 @@
 package models
 
 import (
+	"context"
 	"db-server/meta"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/google/uuid"
+	"io"
+	"os"
+	"strings"
 )
+
+//  cf := models.CloudFunction{
+//		Container: "docker.io/library/alpine",
+//		Params:    []string{"echo", "hello world"},
+//	}
+//
+//	cf.Run()
 
 type CloudFunction struct {
 	Id        uuid.UUID `gorm:"primarykey" json:"id"`
 	Title     string    `json:"title"`
 	Container string    `json:"container"`
-	Params    string    `json:"params"`
+	Params    []string  `json:"params"`
+}
+
+type containerUri struct {
+	Host    string
+	Vendor  string
+	Image   string
+	Version string
+}
+
+func getContainerUri(source string) containerUri {
+
+	uri := containerUri{}
+	parts := strings.Split(source, ":")
+
+	if len(parts) == 2 {
+		uri.Version = parts[1]
+	}
+
+	pathParts := strings.Split(parts[0], "/")
+
+	uri.Host = pathParts[0]
+	uri.Vendor = pathParts[1]
+	uri.Image = pathParts[2]
+
+	return uri
 }
 
 func (p CloudFunction) List(limit int, offset int, sort string, order string) []interface{} {
@@ -49,4 +89,57 @@ func (p CloudFunction) GetById(id string) interface{} {
 func (p CloudFunction) Delete(id string) {
 	conn := meta.MetaDb.GetConnection()
 	conn.Where("id = ?", id).Delete(&p)
+}
+
+func (p CloudFunction) Run() {
+
+	uri := getContainerUri(p.Container)
+
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+
+	path := uri.Host + "/" + uri.Vendor + "/" + uri.Image
+
+	// Делаем docker pull
+	reader, err := cli.ImagePull(ctx, path, types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(os.Stdout, reader)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: uri.Image,
+		Cmd:   p.Params,
+	}, nil, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+
 }
